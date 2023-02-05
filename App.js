94cda8f9991960681d808997f290fcb347c5a266
Fajar <fajarmh2@gmail.com>
2023-02-06 02:27:59 +0700

    add image picker and print successfully

--- App.js
+++ App.js
@@ -2,18 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
-
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceHolderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
           <ImageViewer placeholderImageSource={PlaceHolderImage} />
       </View>
       <View style={styles.footerContainer}>
-          <Button theme="primary" label="Choose a photo" />
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
           <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
