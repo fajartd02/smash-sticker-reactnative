2abafd74953921c10ecfbc8f303948423452b742
Fajar <fajarmh2@gmail.com>
2023-02-18 00:10:48 +0700

    create drag and double tap emoji

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -5,12 +5,15 @@ import Animated, {
 	useAnimatedGestureHandler,
 	withSpring,
 } from 'react-native-reanimated';
-import { TapGestureHandler } from 'react-native-gesture-handler';
+import { PanGestureHandler, TapGestureHandler } from 'react-native-gesture-handler';
 
 const AnimatedImage = Animated.createAnimatedComponent(Image);
+const AnimatedView = Animated.createAnimatedComponent(View);
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
   const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
   
   const onDoubleTap = useAnimatedGestureHandler({
 	onActive: () => {
@@ -20,6 +23,17 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
 	},
   });
 
+  const onDrag = useAnimatedGestureHandler({
+    onStart: (event, context) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.translateX;
+      translateY.value = event.translationY + context.translateY;
+    },
+  });
+
   const imageStyle = useAnimatedStyle(() => {
 	return {
 		width: withSpring(scaleImage.value),
@@ -27,16 +41,30 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
 	};
   });
 
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
 
   return (
-    <View style={{ top: -350 }}>
-	  <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
-        <AnimatedImage
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </TapGestureHandler>
-    </View>
+    <PanGestureHandler onGestureEvent={onDrag}>
+      <AnimatedView style={[containerStyle, { top: -350 }]}>
+        <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+          <AnimatedImage
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </TapGestureHandler>
+      </AnimatedView>
+    </PanGestureHandler>
   );
 }
